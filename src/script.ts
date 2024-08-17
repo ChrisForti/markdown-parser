10c73633f2a869f23d9f26625576826d351b1ac2
Christopher Forti <christopher.forti.79@gmail.com>
2024-08-16 20:58:31 -0400

    Initial commit.

---
+++ src/script.ts
@@ -0,0 +1,20 @@
+import { readFileSync } from "fs";
+import splitMetadataFromMDContent from "parse-md";
+
+type Metadata = {
+  published: boolean;
+  title: string;
+  description: string;
+  date: Date;
+  slug: string;
+};
+
+const myFile = readFileSync("content/test.md", "utf-8");
+const { metadata, content } = splitMetadataFromMDContent(myFile) as {
+  metadata: Metadata;
+  content: string;
+};
+
+if (typeof metadata.published != "boolean") {
+  throw new Error("The markdown must contain a published boolean.");
+}
